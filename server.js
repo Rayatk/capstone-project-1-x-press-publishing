b4a62771c25bae142384bd8b383e1d536b0ec97b
Rayatk <rayatkibria1999@gmail.com>
2024-01-22 03:25:48 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan');
+const express = require('express');
+const apiRouter = require('./api/api');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(errorHandler());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+    console.log('Listen on port: ' + PORT);
+});
+
+module.exports = app;
